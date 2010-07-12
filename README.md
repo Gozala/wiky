f697a393db6fba1b83b3472ea0a4b797b6a91137
Irakli Gozalishvili <rfobic@gmail.com>
2010-07-12 04:07:37 +0200

    Porting wiky to CommonJS package

---
+++ README.md
@@ -0,0 +1,24 @@
+Wiky: A Bidirectional Markup Converter
+======================================
+
+Wiky is a [CommonJS] port of same named clientside [Wiki] markup to HTML
+converter library written in javascript. As it is bidirectional, it can
+convert [Wiki] markup to [HTML] and later convert that generated [HTML] text
+back to [Wiki] markup. Optionally [Wiky] will create math formulas from a
+simple notation similar to LaTeX.
+
+![Wiky example text and output](http://goessner.net/articles/wiky/Wiky01.png)
+
+Wiky is neither an editor nor a widget, though it might be used by these. It
+is just a converter.
+
+You can play with it in the [WikyBox]  
+You can find original project [here][wiky]
+
+Package is available freely under the Creative Commons GNU LGPL License.
+
+[CommonJS]:http://wiki.commonjs.org/ "CommonJS"
+[WikyBox]:http://gozala.github.com/wiky/ "WikyBox"
+[wiky]:http://goessner.net/articles/wiky/ "wiky homepage"
+[wiki]:http://en.wikipedia.org/wiki/Wiki
+[JavaScript]:http://en.wikipedia.org/wiki/JavaScript
---
+++ demo/main.js
@@ -0,0 +1,60 @@
+var Wiky = require('wiky/wiky')
+
+global.output = {
+   menu: "outmnu",
+   item: 0,
+   inbox: null,
+   outbox: null,
+   init: function() { 
+     this.inbox = document.getElementById("inbox");
+     this.outbox = document.getElementById("outbox");
+     this.inbox.curScroll = this.inbox.scrollTop;
+     this.toggle(0);
+   },
+   onkeyup: [
+     function() { output.outbox.innerHTML = Wiky.toHtml(output.inbox.value); output.scroll(); },
+     function() { output.outbox.innerHTML = Wiky.toHtml(output.inbox.value).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/ /g,"&nbsp;").replace(/(\r?\n)/g,"<br/>"); output.scroll(); },
+     function() { output.outbox.innerHTML = Wiky.toWiki(Wiky.toHtml(output.inbox.value)).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/ /g,"&nbsp;").replace(/(\r?\n)/g,"<br/>"); output.scroll(); }
+   ],
+   toggle: function(idx) { 
+     var items = document.getElementById(this.menu).getElementsByTagName("a");
+     items[this.item].className = "";
+     items[this.item=idx].className = "active";
+     this.outbox.style.fontFamily = idx==0 ? "serif" : "monospace";
+     this.inbox.onkeyup = this.inbox.onfocus = this.onkeyup[idx];
+     this.inbox.onmouseup = function(){output.scroll();}
+     this.inbox.focus();
+   },
+   scroll: function() {
+     if (this.inbox.curScroll != this.inbox.scrollTop) {
+        this.inbox.curScroll = this.inbox.scrollTop;
+        this.outbox.scrollTop = this.outbox.scrollHeight/this.inbox.scrollHeight*this.inbox.scrollTop;
+     }
+   }
+};
+global.helpMenu = {
+   menu: "helpmnu",
+   item: 0,
+   toggle: function(idx) {
+     var items = document.getElementById(this.menu).getElementsByTagName("a");
+     document.getElementById(items[this.item].innerHTML).style.display = "none";
+     items[this.item].className = "";
+     items[this.item=idx].className = "active";
+     document.getElementById(items[this.item].innerHTML).style.display = "block";
+   }
+};
+global.example = function example(id) {
+   document.getElementById('inbox').value = document.getElementById(id).innerHTML
+                                                    .replace(/&amp;/g,'&')
+                                                    .replace(/&lt;/g,'<')
+                                                    .replace(/&gt;/g,'>')
+                                                    .replace(/&quot;/g,'\"');
+   window.output.inbox.focus();
+}
+
+exports.main = function main(scenario) {
+    if (document.readyState == "complete") output.init();
+    else window.addEventListener("load", main, false);
+}
+
+if (require.main == module) exports.main();
